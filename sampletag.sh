4ad98bce598d868899c9caf9d329d4ad29a61b4f
VinodBavanasi <105513444+VinodBavanasi@users.noreply.github.com>
2022-06-21 14:41:48 +0530

    Update sampletag.sh

--- sampletag.sh
+++ sampletag.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 echo "Verifying the latest tag checkout"
-sshpass -p 'B@ngalore123' ssh -o StrictHostKeyChecking=no -l git fetch https://gitlab.smartfren.com/sinarmasdigital-tio/mysf/live --tags ;
+git fetch https://gitlab.smartfren.com/sinarmasdigital-tio/mysf/live --tags ;
 latesttag=$(git describe --tags `git tag --sort=-creatordate | head -n 1`) && branch=preprod-v3n && echo \’${branch}\’ ;
 echo ${latesttag}
 git checkout $latesttag -b $branch
